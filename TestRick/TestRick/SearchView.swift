634466fc156c7a1dbaa226451fdf9ce331f986a8
Kurilova Daria Kirillovna <51517187+novembrz@users.noreply.github.com>
2023-05-07 16:28:49 +0500

    Lesson 2: SearchView grid

--- TestRick/TestRick/SearchView.swift
+++ TestRick/TestRick/SearchView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct SearchView: View {
+    let persons = ["Picture", "Picture 1", "Picture 2", "Picture 3"]
+    let columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: 18), count: 3)
+    
     var body: some View {
         NavigationView {
-            Text("Search")
-                .navigationTitle("Rickipedia")
+            LazyVGrid(columns: columns, spacing: 23) {
+                ForEach(persons, id: \.self) { person in
+                    VStack {
+                        Image(person)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(height: 145)
+                            .frame(minWidth: 90, maxWidth: 125, alignment: .center)
+                            .clipped()
+                            .cornerRadius(15)
+                        
+                        Text(person)
+                            .foregroundColor(.black)
+                            .font(.system(size: 12, weight: .regular))
+                    }
+                }
+            }
+            .padding(.horizontal, 20)
+            .navigationTitle("Rickipedia")
+            .background(Color("BackgroundColor"), ignoresSafeAreaEdges: .all)
         }
-       
     }
 }
 
